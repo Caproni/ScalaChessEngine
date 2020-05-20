f04c8ec9d8bb6e259b885082474ffa597bc26fba
Edmund Bennett <Caproni@users.noreply.github.com>
2020-05-20 21:20:54 +0100

    Delete OhmsLaw.scala

--- src/main/scala/com/huginnmuninnresearch/electrical/OhmsLaw.scala
+++
@@ -1,7 +0,0 @@
-package com.huginnmuninnresearch.electrical
-
-object OhmsLaw {
-  def calcV(current: Double, resistance: Double): Double = current * resistance
-  def calcI(voltage: Double, resistance: Double): Double = voltage / resistance
-  def calcR(voltage: Double, current: Double): Double = voltage / current
-}
