7e0e21521b4e437450b405a9e45531831f3c5630
Edmund Bennett <Caproni@users.noreply.github.com>
2020-05-20 21:20:47 +0100

    Delete PowerRatioDB.scala

--- src/main/scala/com/huginnmuninnresearch/electrical/PowerRatioDB.scala
+++
@@ -1,8 +0,0 @@
-package com.huginnmuninnresearch.electrical
-
-object PowerRatioDB {
-
-  def calcDBFromPower(p1: Double, p2: Double): Double = 10*Math.log10(p2/p1)
-  def calcDBFromAmplitude(a1: Double, a2: Double): Double = 20*Math.log10(a2/a1)
-
-}
